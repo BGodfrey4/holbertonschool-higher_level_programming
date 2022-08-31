d99ff641861461bec33872cec415e9799953022c
bgodfrey4 <bgodfrey94@gmail.com>
2022-08-30 21:18:57 -0700

    answers

---
+++ 0x00-python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print("1st 3 letters: {}".format(word_first_3))
+print("The Last 2 letters: {}".format(word_last_2))
+print("The middle word: {}".format(middle_word))
