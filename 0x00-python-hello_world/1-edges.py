a4393ed32896d42ae647b05b65dd1de892a2b630
bgodfrey4 <bgodfrey94@gmail.com>
2022-08-30 21:29:41 -0700

    corrections

--- 0x00-python-hello_world/1-edges.py
+++ 0x00-python-hello_world/1-edges.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 word = "Holberton"
+word_first_3 = word[:2]
+word_last_2 = word[1:]
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print("1st 3 letters: {}".format(word_first_3))
-print("The Last 2 letters: {}".format(word_last_2))
-print("The middle word: {}".format(middle_word))
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
