2ef49d375299e47955bc57e31fc62aae631ba6bc
bgodfrey4 <bgodfrey94@gmail.com>
2022-09-02 21:53:42 -0700

    answers

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+
+
 def safe_print_division(a, b):
-try:
-value = a / b
-except ZeroDivisionError:
-value = "None"
-finally:
-print("Inside result: {}".format(value))
-return(value)
+    while True:
+        try:
+            a / b
+            break
+    except ZeroDivisionError:
+            break
+        finally:
+    if b != 0:
+        print("Inside result: {}".format(a / b))
+        return (a / b)
+    else:
+        print("Inside result: {}".format(None))
