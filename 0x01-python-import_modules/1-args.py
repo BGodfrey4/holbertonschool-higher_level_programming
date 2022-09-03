429529b213ce25387895a950fbe16dfb837d7d61
bgodfrey4 <bgodfrey94@gmail.com>
2022-09-02 21:44:09 -0700

    answers

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    
-    a = len(sys.argv)
-   
-    if a == 1:
-        print("0 arguments.")
-	elif a == 2:
-        print("1 argument:")
-    print("1: {:s}".format(sys.argv[1]))
+
+    argv = sys.argv[1:]
+    num_of_args = len(argv)
+    arg_count = 1
+
+    if num_of_args < 1:
+        print("{:d} arguments.".format(num_of_args))
+    elif num_of_args == 1:
+        print("{:d} argument:".format(num_of_args))
     else:
-	print("{:d} arguments:".format(a-1))
-    for i in range(1, a):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{:d} arguments:".format(num_of_args))
+    for i in argv:
+        print("{:d}: {}".format(arg_count, i))
+        arg_count += 1
