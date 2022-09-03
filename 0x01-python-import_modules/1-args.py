ba3cc2e1fbbfdc47b7857ec8a68e91511bc28d21
bgodfrey4 <bgodfrey94@gmail.com>
2022-09-02 21:28:44 -0700

    corrections

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -3,11 +3,12 @@ if __name__ == '__main__':
     import sys
     a = len(sys.argv)
     if a == 1:
-    print("0 arguments.")
+    	print("0 arguments.")
+		break
     elif a == 2:
-    print("1 argument:")
+    	print("1 argument:")
     print("1: {:s}".format(sys.argv[1]))
     else:
-    print("{:d} arguments:".format(a-1))
+        print("{:d} arguments:".format(a-1))
     for i in range(1, a):
-    print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{:d}: {:s}".format(i, sys.argv[i]))
