f7626ee8261dc5ed8220c572c8c86323427b1f94
bgodfrey4 <bgodfrey94@gmail.com>
2022-09-08 21:42:37 -0700

    corrections

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    """
+""" writes a class square"""
 
-    def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
+
+class Square:
+    """attempt to model a square"""
+
+    def __init__(self, size=0):  # instantiation happens here
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
