1209c123ea2c8279b1b5e399cfc216cf16d08ddd
bgodfrey4 <bgodfrey94@gmail.com>
2022-09-08 21:14:50 -0700

    corrections

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
-    """
+"""A simple Square class that specifies a square by the size."""
 
-    def __init__(self, size):
-        """Initializes the data."""
+
+class Square:
+    """A simple Square class that specifies the square by the size.
+    Attributes:
+        __size (int): Is the size of the square.
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size is always an integer")
+        if size < 0:
+            raise ValueError("size is always >= 0")
         self.__size = size
+
+    def area(self):
+        """Will find the area of the square.
+        
+        Returns:
+            int : Is just the area of the square.
+        """
+        return self.__size ** 2
