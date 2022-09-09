fcdda2a51498c43db4aceebd27b9c59d50778207
bgodfrey4 <bgodfrey94@gmail.com>
2022-09-08 21:04:29 -0700

    corrections

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""A simple Square class specifies a square by the size."""
+
+
 class Square:
-    def __init__(self, size):
+    """A simple Square class that specifies a square by the size.
+    Attributes:
+        __size (int): Is the size of the square.
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
