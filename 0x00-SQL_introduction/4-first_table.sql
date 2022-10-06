73bf7ed3170409f79218eada20d2642a0bba231c
bgodfrey4 <bgodfrey94@gmail.com>
2022-10-05 21:41:00 -0700

    answers

---
+++ 0x00-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- creates table called first_table in current database in MySQL server
+-- creates a table even if the table already exists
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256))
