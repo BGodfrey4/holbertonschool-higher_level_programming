a68e0e1bc9d12bca30c2c104ff1d4b6b6e444705
bgodfrey4 <bgodfrey94@gmail.com>
2022-10-06 21:51:36 -0700

    answers

---
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- lists shows contained in hbtn_0d_tvshows that at least one genre linked
+-- lists all rows of a database that have one column in common
+SELECT tv_shows.title, FROM tv_shows INNER JOIN tv_show_genres
