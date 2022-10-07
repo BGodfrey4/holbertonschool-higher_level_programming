0496177a83335d3e03f5e8a45210c54f72a1d13e
bgodfrey4 <bgodfrey94@gmail.com>
2022-10-06 21:05:10 -0700

    corrections

---
+++ 0x01-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- lists all privileges of MySQL users user_0d_1 and user_0d_2 on server
+-- lists all granted privileged for one user in a database
+SHOW GRANTS FOR user_0d_1@localhost
+-- lists all granted privileged for one user in a database
+SHOW GRANTS FOR user_0d_2@localhost
