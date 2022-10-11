b53af5528d9f78f0ad434125f24ab02ab3094708
bgodfrey4 <bgodfrey94@gmail.com>
2022-10-10 20:23:42 -0700

    corrections

--- 0x00-SQL_introduction/14-average.sql
+++ 0x00-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
- the score average of records in table second_table of the database hbtn_0c_0 
- -- computes an average of scores in a table
- SELECT AVG(`score`) 'average' FROM second_table
+-- computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server
+-- computes an average of scores in a table
+SELECT AVG(`score`) 'average' FROM second_table;
