7f73e45d6ffe77e642af26be34bab094c505bf9d
bgodfrey4 <4936@holbertonschool.com>
2022-12-03 03:46:10 +0000

    answers

--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (_w, _h) {
-    if ( _w > 0 && _h > 0 ) {
-      this.width = _w;
-      this.height = _h;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = parseInt(w);
+      this.height = parseInt(h);
     }
   }
+
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log("X".repeat(this.width));
-    }
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
