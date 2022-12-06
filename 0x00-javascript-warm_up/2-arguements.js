066dfca233ceec644fabee53574100a80f998487
bgodfrey4 <4936@holbertonschool.com>
2022-12-06 05:29:57 +0000

    corrections

--- 0x00-javascript-warm_up/2-arguements.js
+++ 0x00-javascript-warm_up/2-arguements.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const process = require('process');
-let args = process.argv.slice(2);
-let n = args.length;
-if (n === 0) {
+if (process.argv.length < 3) {
   console.log('No argument');
-  } else if (n === 1) {
-      console.log('Arguments found');
-    } else {
-    console.log('Arguments found');
+} else if (process.argv.length === 3) {
+    console.log('Argument found');
+} else{console.log('Arguments found');
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
