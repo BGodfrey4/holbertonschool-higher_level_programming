06c83848878e66c88c81e05aac3884f6d5da52e7
bgodfrey4 <4936@holbertonschool.com>
2023-03-22 21:49:57 -0700

    tasks

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-"""Sends post request/email to url and displays"""
-import sysimport urllib.request
-if _name_ == "_main_":    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:        headers = response.info()        x_request_id = headers['X-Request-Id']
+"""This code is a Python script that takes
+a URL as an argument and sends a request to the URL.
+It then displays the value of the X-Request-ID
+header from the response.
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        x_request_id = headers['X-Request-Id']
+
     print(x_request_id)
